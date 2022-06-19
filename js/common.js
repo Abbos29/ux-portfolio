a77a55b44977b3c265354c456f8818a55cade5f1
Abbos29 <78086415+Abbos29@users.noreply.github.com>
2022-06-19 17:39:07 +0500

    feat: add aos.js (animate)

--- js/common.js
+++ js/common.js
@@ -1,6 +1,11 @@
+AOS.init();
+
+
 const navToggle = document.querySelector(".burger__menu");
 const mainMenu = document.querySelector(".nav");
 const tBody = document.querySelector("body");
+const langRuToggle = document.querySelector(".lang__ru");
+const langEngToggle = document.querySelector(".lang__eng");
 
 navToggle.addEventListener("click", () => {
   mainMenu.classList.toggle("burger__active");
@@ -11,5 +16,29 @@ mainMenu.addEventListener("click", (event) => {
   if(event.target){
     mainMenu.classList.remove("burger__active");
     tBody.classList.remove("lock");
+  }
+});
+
+langRuToggle.addEventListener("click", () => {
+  langRuToggle.classList.toggle("avtive-lang");
+  langRuToggle.style.color = '#070707';
+});
+
+langRuToggle.addEventListener("click", (event) => {
+  if(event.target){
+    langEngToggle.classList.remove("active-lang");
+    langEngToggle.style.color = 'inherit';
+  }
+});
+
+langEngToggle.addEventListener("click", () => {
+  langEngToggle.classList.toggle("avtive-lang");
+  langEngToggle.style.color = '#070707';
+});
+
+langEngToggle.addEventListener("click", (event) => {
+  if(event.target){
+    langRuToggle.classList.remove("active-lang");
+    langRuToggle.style.color = 'inherit';
   }
 });
--- js/dist/common.dev.js
+++ js/dist/common.dev.js
@@ -1,8 +1,11 @@
 "use strict";
 
+AOS.init();
 var navToggle = document.querySelector(".burger__menu");
 var mainMenu = document.querySelector(".nav");
 var tBody = document.querySelector("body");
+var langRuToggle = document.querySelector(".lang__ru");
+var langEngToggle = document.querySelector(".lang__eng");
 navToggle.addEventListener("click", function () {
   mainMenu.classList.toggle("burger__active");
   tBody.classList.toggle("lock");
@@ -11,5 +14,25 @@ mainMenu.addEventListener("click", function (event) {
   if (event.target) {
     mainMenu.classList.remove("burger__active");
     tBody.classList.remove("lock");
+  }
+});
+langRuToggle.addEventListener("click", function () {
+  langRuToggle.classList.toggle("avtive-lang");
+  langRuToggle.style.color = '#070707';
+});
+langRuToggle.addEventListener("click", function (event) {
+  if (event.target) {
+    langEngToggle.classList.remove("active-lang");
+    langEngToggle.style.color = 'inherit';
+  }
+});
+langEngToggle.addEventListener("click", function () {
+  langEngToggle.classList.toggle("avtive-lang");
+  langEngToggle.style.color = '#070707';
+});
+langEngToggle.addEventListener("click", function (event) {
+  if (event.target) {
+    langRuToggle.classList.remove("active-lang");
+    langRuToggle.style.color = 'inherit';
   }
 });
